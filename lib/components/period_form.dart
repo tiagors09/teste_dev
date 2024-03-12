9429085eb8611c205bf1e140206a76faac6ce76a
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2024-03-12 16:11:34 -0300

    added padding in fields and dropdown selector

--- lib/components/period_form.dart
+++ lib/components/period_form.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:teste_dev/components/date_selection_button.dart';
+import 'package:teste_dev/components/dropdown_selector.dart';
 
 class PeriodForm extends StatefulWidget {
   const PeriodForm({super.key});
@@ -17,42 +18,68 @@ class _PeriodForm extends State<PeriodForm> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: MediaQuery.of(context).size.width * 0.2,
-      height: MediaQuery.of(context).size.height * 0.3,
-      margin: const EdgeInsets.all(2.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          TextField(
-            controller: titleCtrl,
-            decoration: const InputDecoration(
-              label: Text(
-                'Nomeie seu periodo',
+    return Expanded(
+      child: Container(
+        width: MediaQuery.of(context).size.width * 0.20,
+        margin: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                controller: titleCtrl,
+                decoration: const InputDecoration(
+                  label: Text(
+                    'Nomeie seu periodo',
+                  ),
+                ),
               ),
             ),
-          ),
-          DateSelectionButton(
-            label: 'Começa',
-            date: _startDate,
-            onDateChange: (newDate) => setState(() {
-              _startDate = newDate;
-            }),
-          ),
-          DateSelectionButton(
-            label: 'Termina',
-            date: _endsDate,
-            onDateChange: (newDate) => setState(() {
-              _endsDate = newDate;
-            }),
-          ),
-          ElevatedButton(
-            onPressed: () {},
-            child: const Text(
-              'Concluir',
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: DateSelectionButton(
+                label: 'Começa',
+                date: _startDate,
+                onDateChange: (newDate) => setState(() {
+                  _startDate = newDate;
+                }),
+              ),
             ),
-          )
-        ],
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: DateSelectionButton(
+                label: 'Termina',
+                date: _endsDate,
+                onDateChange: (newDate) => setState(() {
+                  _endsDate = newDate;
+                }),
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: DropdownSelector(label: 'Categoria'),
+            ),
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: DropdownSelector(
+                label: 'Meta 1',
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: DropdownSelector(
+                label: 'Meta 2',
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text(
+                'Concluir',
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
