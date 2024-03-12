7f41d5559a7ba06032ae7f38b080cf368228f40d
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2024-03-12 13:59:07 -0300

    created a date selection button

---
+++ lib/components/date_selection_button.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class DateSelectionButton extends StatelessWidget {
+  final DateTime date;
+  final void Function(DateTime) onDateChange;
+  final String label;
+
+  const DateSelectionButton({
+    super.key,
+    required this.date,
+    required this.onDateChange,
+    required this.label,
+  });
+
+  _openDatePicker(BuildContext ctx) {
+    showDatePicker(
+      context: ctx,
+      firstDate: DateTime(1999),
+      lastDate: DateTime.now(),
+    ).then(
+      (newDate) => onDateChange(newDate!),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          label,
+        ),
+        TextButton(
+          onPressed: () => _openDatePicker(context),
+          child: Text(DateFormat('dd/MM/yyyy').format(date)),
+        ),
+      ],
+    );
+  }
+}
